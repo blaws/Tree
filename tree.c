cffbf03546495cdb0b6d44b88b4057b753c165dc
blaws <blaws@nd.edu>
2013-05-20 21:18:57 -0400

    Added leaves

--- tree.c
+++ tree.c
@@ -2,13 +2,31 @@
 #include <math.h>
 #include "gfx.h"
 
+void leafdraw(int x,int y,double thetaDeg){
+  double theta = thetaDeg * M_PI/180;  // convert angle to radians
+  int i,r=0,dir=2,rmax=5;
+  gfx_color(rand()%20,175+pow(-1,rand()%2)*(rand()%20),rand()%20);
+  gfx_line(x,y,x+15*sin(theta),y+15*cos(theta)); // draw main stem
+
+  for(i=0;i<15;i++){  // draw points
+    gfx_line(x+i*sin(theta),y+i*cos(theta),x+i*sin(theta)+r*sin(theta+M_PI/4),y+i*cos(theta)+r*cos(theta+M_PI/4));
+    gfx_line(x+i*sin(theta),y+i*cos(theta),x+i*sin(theta)+r*sin(theta-M_PI/4),y+i*cos(theta)+r*cos(theta-M_PI/4));
+    r+=dir;
+    if(r==0 || r==rmax){
+      dir*=-1;
+      rmax--;
+    }
+  }
+}
+
 void branchdraw(double x,double y,int xmax,int ymax,int r,double thetaDeg,int level){
-  if(level>3 || r<1 || y<0 || y>ymax || x>xmax) return;
+  if(level>3 || y<0 || y>ymax || x>xmax) return;
+  else if(r<1){
+    leafdraw(x,y,thetaDeg);
+    return;
+  }
   double i;
   double theta = thetaDeg * M_PI/180;  // convert angle to radians
-
-  /*for(i=0;i<level;i++) printf(" ");
-    printf("New branch.\n");*/
 
   while(r>0){
     // draw current "layer" of tree
@@ -16,6 +34,9 @@ void branchdraw(double x,double y,int xmax,int ymax,int r,double thetaDeg,int le
       gfx_color(139+pow(-1,rand()%2)*(rand()%20),69+pow(-1,rand()%2)*(rand()%20),19+pow(-1,rand()%2)*(rand()%20));  // base color: 139,69,19 (light brown)
       gfx_line(x+i*cos(theta),y+i*sin(theta),x+(i+.5)*cos(theta),y+(i+.5)*sin(theta));
     }
+
+    // draw leaf if branch is thin enough
+    if(r<3 && rand()%10<1) leafdraw(x,y,thetaDeg+pow(-1,rand()%2)*(45+rand()%90));
 
     // move to next layer
     y -= cos(theta);
@@ -35,7 +56,7 @@ void branchdraw(double x,double y,int xmax,int ymax,int r,double thetaDeg,int le
       double theta2 = thetaDeg+pow(-1,rand()%2)*(rand()%45);
       branchdraw(x-r/2,y+5*cos(theta),xmax,ymax,r/2,theta1<theta2?theta1:theta2,level);
       branchdraw(x+r/2,y+5*cos(theta),xmax,ymax,r/2,theta1>=theta2?theta1:theta2,level);
-      break;
+      return;
     }
 
     if((rand()%(4*r*r+1>10?4*r*r+1:10)) < 1){  // make new branch off current branch (modulus by 10 or greater)
@@ -45,6 +66,7 @@ void branchdraw(double x,double y,int xmax,int ymax,int r,double thetaDeg,int le
       branchdraw(x,y,xmax,ymax,r-rand()%(r/2+1),newTheta,level+1);
     }
   }
+  if(rand()%10<9) leafdraw(x,y,thetaDeg);  // put leaf at end of branch
 }
 
 int main(void){
